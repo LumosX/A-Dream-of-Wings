2ba58af46c8d17fad1be91af2573334c12961077
Radoslav <radmrkv@gmail.com>
2019-10-05 19:58:12 +0300

    UI for dialogues seems to be in place now. Gotta fix up the callbacks and make the UI controller responsible for it.

---
+++ Assets/DialogueScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScreen : MonoBehaviour {
+
+    public Text dialogueTextbox;
+    public Button[] buttons;
+    public Text[] buttonTextBoxes;
+
+
+    public void SetDialogueInfo(string text, params string[] choices) {
+        dialogueTextbox.text = text;
+        
+        for (var i = 0; i < 4; i++) {
+            if (i < choices.Length) {
+                buttons[i].gameObject.SetActive(true);
+                buttonTextBoxes[i].text = choices[i];
+            }
+            else {
+                buttons[i].gameObject.SetActive(false);
+                buttonTextBoxes[i].text = "";
+            }
+        }
+    }
+
+    public void OnClick_DialogueButton(int optionSelected) {
+        // NOTE: optionSelected is ONE-BASED, NOT ZERO-BASED!
+        
+    }
+    
+}
